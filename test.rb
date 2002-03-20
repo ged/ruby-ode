3fa82f1bad9a4fc9f5d74f6640c28ae585870ef9
Michael Granger <ged@FaerieMUD.org>
2002-03-20 05:56:39 +0000

    - Converted to use Test::Unit
    --HG--
    extra : convert_revision : svn%3A1cb8edaa-bce6-0310-82d4-a8f298a11cf6/trunk%4011

--- test.rb
+++ test.rb
@@ -4,28 +4,59 @@
 #
 #
 
-require "walkit/cli_script"
-require "find"
 
-$: << "src"
+$LOAD_PATH.unshift "src", "ext"
+
+require 'find'
+require 'test/unit'
+require 'test/unit/testsuite'
+require 'test/unit/ui/console/testrunner'
+
+require 'ode'
+
+if ARGV.length.nonzero?
+	patsrc = "(" + ARGV.join('|') + ")"
+	$stderr.puts "Looking for tests matching /#{patsrc}/"
+	pattern = /#{patsrc}/i
+else
+	pattern = nil
+end
 
 ### Load all the tests from the tests dir
-puts "Finding test files..."
-Find.find("t") {|file|
-	Find.prune if file =~ %r{^\.\.|.*/\.}
+Find.find("tests") {|file|
+	Find.prune if file =~ %r{(^|\/)[\.#]} or file =~ /~$/
+	Find.prune if file =~ /TEMPLATE/
 	next if File.stat( file ).directory?
-	next unless file =~ /.*_tests\.rb$/
-	puts "  Requiring #{file}..."
-	require( file )
+	next unless file =~ /_tests.rb$/
+	next if pattern && pattern !~ file
+
+	$stderr.puts "Found '#{file}'"
+	require "#{file}"
 }
 
-### Find all the loaded test classes
-testClasses = []
-ObjectSpace.each_object( Class ) {|klass|
-	next unless klass < Walkit::Testclass
-	testClasses << klass
-}
+class MUESTests
+	class << self
+		def suite
+			suite = Test::Unit::TestSuite.new( "Ruby-ODE" )
+			classes = []
 
-### Run 'em, or run the ones that match Regexp.new(ARGV[0]), if specified.
-Walkit::Cli_script.new.select( testClasses, $*.shift )
+			ObjectSpace.each_object( Class ) {|klass|
+				if klass < Test::Unit::TestCase
+					classes << klass
+				end
+			}
+
+			$stderr.puts "WARNING: No matching testcases found." if classes.empty?
+			classes.each {|klass|
+				suite.add( klass.suite )
+			}
+			return suite
+		end
+	end
+end
+
+Test::Unit::UI::Console::TestRunner.new( MUESTests ).start
+
+
+
 
