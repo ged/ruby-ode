d7f54b5b244518ffb7a63f9ee84edd8817289cd3
Michael Granger <ged@FaerieMUD.org>
2002-11-23 22:56:00 +0000

    - Tailored to ODE testing.
    - Added an experimental FXRuby testrunner.
    
    --HG--
    extra : convert_revision : svn%3A1cb8edaa-bce6-0310-82d4-a8f298a11cf6/trunk%4039

--- test.rb
+++ test.rb
@@ -4,15 +4,16 @@
 #
 #
 
-
-$LOAD_PATH.unshift "src", "ext"
+$LOAD_PATH.unshift "lib", "ext"
 
 require 'find'
 require 'test/unit'
 require 'test/unit/testsuite'
-require 'test/unit/ui/console/testrunner'
-
-require 'ode'
+# begin
+# 	require 'test/unit/ui/fox/testrunner'
+# rescue
+	require 'test/unit/ui/console/testrunner'
+#end
 
 if ARGV.length.nonzero?
 	patsrc = "(" + ARGV.join('|') + ")"
@@ -27,35 +28,53 @@ Find.find("tests") {|file|
 	Find.prune if file =~ %r{(^|\/)[\.#]} or file =~ /~$/
 	Find.prune if file =~ /TEMPLATE/
 	next if File.stat( file ).directory?
-	next unless file =~ /_tests.rb$/
+	next unless file =~ /tests.rb$/
 	next if pattern && pattern !~ file
 
 	$stderr.puts "Found '#{file}'"
 	require "#{file}"
 }
 
-class MUESTests
-	class << self
-		def suite
-			suite = Test::Unit::TestSuite.new( "Ruby-ODE" )
-			classes = []
+class ODETests
 
-			ObjectSpace.each_object( Class ) {|klass|
-				if klass < Test::Unit::TestCase
-					classes << klass
-				end
-			}
+	def initialize
+		@suite = Test::Unit::TestSuite.new( "Ruby-ODE" )
+		classes = []
 
-			$stderr.puts "WARNING: No matching testcases found." if classes.empty?
-			classes.each {|klass|
-				suite.add( klass.suite )
-			}
-			return suite
-		end
+		ObjectSpace.each_object( Class ) {|klass|
+			if klass < Test::Unit::TestCase
+				classes << klass
+			end
+		}
+
+		$stderr.puts "WARNING: No matching testcases found." if classes.empty?
+		classes.each {|klass|
+			@suite.add( klass.suite )
+		}
+	end
+
+	attr_accessor :suite
+
+	def size
+		self.suite.size
+	end
+
+	def run( *args )
+		self.suite.run( *args )
+	end
+
+	def to_s
+		"Ruby-ODE Test Suite (%d tests)" % self.size
 	end
 end
 
-Test::Unit::UI::Console::TestRunner.new( MUESTests ).start
+
+if ENV.key?( "DISPLAY" ) && Object::const_defined?( :Fox )
+	Test::Unit::UI::Fox::TestRunner.run( ODETests::new )
+else
+	Test::Unit::UI::Console::TestRunner.run( ODETests::new )
+end
+
 
 
 
